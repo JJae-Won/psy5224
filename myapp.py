7974b16901184109ba1509ecc1cc748de3d31ba0
Jaewon <jwjang2@superbin.co.kr>
2022-07-11 15:25:43 +0900

    aa

--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import cv2
 
 st.write("""
          # 수열이네 필라테스
@@ -6,3 +7,8 @@ st.write("""
          상세문의는 010-5224-2920 전화문자 구독 좋아요 부탁드려용~
          """
          )
+
+image = cv2.imread('./image.png')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+st.image(image)
